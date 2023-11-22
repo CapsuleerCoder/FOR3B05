a4c613eb4e3639ec1b1ffdc1c40fd6653cb1aa17
CapsuleerCoder <oli0202@gmail.com>
2023-11-22 10:46:37 +0000

    Verkefni19 klárað

---
+++ Verkefni/Verkefni019.py
@@ -0,0 +1,64 @@
+#Dæmi A
+# svar = ""
+# while type(svar) is not int:
+#     svar = input("Sláðu inn heiltölu:")
+#     try:
+#         svar = int(svar)
+#         print ("Inntak er í lagi, ")
+#     except ValueError:
+#         print ("Inntak er ekki heiltala. ")
+
+#Dæmi B
+def sum(a, b):
+    try:
+        return a+b
+    except TypeError:
+        return "Rangur innsláttur"
+
+innsláttur1 = "svar"
+innslátttur2 = 3
+
+svar = sum(innsláttur1, innslátttur2)
+print (svar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
