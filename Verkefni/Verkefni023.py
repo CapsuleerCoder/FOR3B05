0258601244679a7a82cbc09dfd281607a0d1c721
CapsuleerCoder <oli0202@gmail.com>
2023-12-05 10:11:00 +0000

    Buinn með verkefni23 git log

--- Verkefni/Verkefni023.py
+++ Verkefni/Verkefni023.py
@@ -1,17 +1,18 @@
+# Dæmi 1
 class Hundur:
     def __init__(self, nafn, aldur):
         self.nafn = nafn
         self.aldur = aldur
 
     def __str__(self):
-        return f"Nafn: {self.nafn}, aldur: {self.aldur}"
+         return f"Nafn: {self.nafn}, aldur: {self.aldur}"
     
     def gelt(self):
-        return print ("voff")
+        return  "\nvoff"
 
-def main(gelt = False): # Næ ekki að fá geltið eftir hundinn. 
+def main(gelt = False):  
     dog = Hundur("Böðvar", "81")
-    return print (dog.gelt() , dog) if gelt == True else print(dog) 
+    print (dog, dog.gelt()) if gelt == True else print(dog) 
 
 svar = input("langar þér að hann Böðvar hinn hrikalegi gelti á þig? j eða n ")
 
@@ -19,3 +20,60 @@ if svar.lower() == "j":
     main(True)
 else:
     main()
+
+#Dæmi 2
+class Nemandi:
+    def __init__ (self, einkunn = 10):
+        self.einkunn = einkunn
+    
+
+    def hækka_einkunn(self):
+        self.einkunn += 10
+
+    def lækka_einkunn(self):
+        self.einkunn -= 10        
+
+    def __str__(self):
+        return f"Einkunn hjá nemenda er {self.einkunn} "
+
+bob = Nemandi()
+print (f"normal {bob}")
+
+bob.hækka_einkunn()
+print (f"hækkar {bob}")
+
+bob.lækka_einkunn()
+print (f"lækkar {bob}")
+#Dæmi C
+class Ferhyrningur:
+    def __init__ (self, lengd, breidd):
+        self.lengd = lengd
+        self.breidd = breidd
+    
+    def flatarmál(self):
+        return self.breidd*self.lengd
+
+    def ummál(self):
+        return ((self.breidd + self.lengd )*2)
+
+    def __str__(self):
+        return f"Lengd: {self.lengd}, breidd: {self.breidd}"
+
+def main():
+    kassinn = Ferhyrningur(5, 5)
+    print (kassinn)
+    ummal = kassinn.ummál()
+    print (f"Ummálið er: {ummal}")
+    print (f"Flatarmálið er: {kassinn.flatarmál()}")
+main()
+
+
+
+
+
+
+
+
+
+
+
