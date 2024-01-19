67def1836a5e1fcc4160acd04f2b4df0a36f6302
CapsuleerCoder <oli0202@gmail.com>
2024-01-19 12:36:53 +0000

    slatti af gömlum filum fór með

--- Sýnidæmi/Synidaemi22.py
+++
@@ -1,9 +0,0 @@
-
-dagar = ["mánudagur", "þriðjudagur", "miðvikudagur", "fimmtudagur", "föstudagur", "laugardagur", "sunnudagur"]
-
-for i in dagar:
-    print(i)
-
-print("virkir dagar eru: ", dagar[:5])
-print("helgardagar eru: ", dagar[5:])
-
